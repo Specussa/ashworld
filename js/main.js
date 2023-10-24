e07737847e055c495d39ca46c43e9c9c4d1c3258
Spec Ussa <serega.ussa@gmail.com>
2023-10-24 18:15:36 +0300

    build

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 // кнопка header__burger
 const burger = document.querySelector('.header__burger');
+const headermenu = document.querySelector('.header__menu');
 burger.addEventListener('click', function() {
   if (burger.classList.contains("active")) {
     burger.classList.remove("active");
+    headermenu.classList.remove("active");
   } else {
     burger.classList.add("active");
+    headermenu.classList.add("active");
   }
 })
 // end header__burger
