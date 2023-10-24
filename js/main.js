68cdb91ee4d51020dae7f1d1e29bcbe2525ababf
Spec Ussa <serega.ussa@gmail.com>
2023-10-24 19:49:20 +0300

    build

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// кнопка header__burger
+// кнопка header burger
 const burger = document.querySelector('.header__burger');
 const headermenu = document.querySelector('.header__menu');
 burger.addEventListener('click', function() {
@@ -10,4 +10,43 @@ burger.addEventListener('click', function() {
     headermenu.classList.add("active");
   }
 })
-// end header__burger
+// end header burger
+
+// start header tabs
+// pdescrbutton.addEventListener('click', function() {
+//   if (!this.classList.contains("active")) {
+//     this.classList.add("active");
+//   }
+// })
+// const pcharacter = document.querySelector('.product__character');
+// pcharacter.style.maxHeight = (pcharacter.scrollHeight * 1) + "px";
+
+const headermenutabs = document.querySelectorAll(".header__menu_tabs");
+[...headermenutabs].forEach(function (li) {
+  for (let [index, elem] of [...li.children].entries()) {
+    elem.setAttribute("data-select-item", index + 1);
+  }
+});
+
+const headermenulists = document.querySelectorAll(".header__menu_lists");
+[...headermenulists].forEach(function (li) {
+  for (let [index, elem] of [...li.children].entries()) {
+    elem.setAttribute("data-select-item", index + 1);
+  }
+});
+
+let headermenutab = document.querySelectorAll(".header__menu_tab");
+for (i = 0; i < headermenutab.length; i++) {
+  headermenutab[i].onclick = function (e) {
+    if (!this.classList.contains("active")) {
+      const headermenutabCOUNT = this.getAttribute("data-select-item");
+      const headermenulistCOUNT = document.querySelector('.header__menu_list[data-select-item="' + headermenutabCOUNT + '"]');
+      headermenutab.forEach((n) => n.classList.remove("active"));
+      document.querySelectorAll(".header__menu_list").forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+      headermenulistCOUNT.classList.add("active");
+      headermenulistCOUNT.style.maxHeight = headermenulistCOUNT.scrollHeight + "px";
+    }
+  };
+}
+// end header tabs
