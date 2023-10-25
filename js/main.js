44c3ea8f9be4f3250ff02e3689ec2ba28cc06a2a
Spec Ussa <serega.ussa@gmail.com>
2023-10-25 13:26:27 +0300

    add media

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// start height
+let oldWidth = window.innerWidth;
+const docheight = document.documentElement
+docheight.style.setProperty('--height', `${window.innerHeight}px`);
+const appHeight = () => {
+  var newWidth = window.innerWidth;
+  if (newWidth != oldWidth) {
+    docheight.style.setProperty('--height', `${window.innerHeight}px`);
+  }
+  oldWidth = window.innerWidth;
+}
+window.addEventListener('resize', appHeight);
+appHeight();
+// end height
+
 // start header tabs
 const headermenutabs = document.querySelectorAll(".header__menu_tabs");
 [...headermenutabs].forEach(function (li) {for (let [index, elem] of [...li.children].entries()) {elem.setAttribute("data-select-item", index + 1);}});
@@ -8,7 +23,7 @@ const headermenulists = document.querySelectorAll(".header__menu_lists");
 const hlisttop = document.querySelectorAll('.header__menu_sublist'); 
 [...hlisttop].forEach(function (li) {for (let [index, elem] of [...li.children].entries()){elem.style.setProperty('--inc-step', index+1);}});
 
-const headermenutab = document.querySelectorAll(".header__menu_tab");
+const headermenutab = document.querySelectorAll(".header__menu_tab_button");
 const headermenulist = document.querySelectorAll(".header__menu_list");
 for (i = 0; i < headermenutab.length; i++) {
   headermenutab[i].onclick = function (e) {
@@ -31,15 +46,17 @@ const burger = document.querySelector('.header__burger');
 const headermenu = document.querySelector('.header__menu');
 burger.addEventListener('click', function() {
   if (burger.classList.contains("active")) {
+    document.documentElement.classList.remove("open");
     burger.classList.remove("active");
     headermenu.classList.remove("active");
     headermenutab.forEach((n) => n.classList.remove("active")); 
     headermenulist.forEach((n) => n.classList.remove("active"));
     headermenulist.forEach((n) => n.style.maxHeight = null);
   } else {
+    document.documentElement.classList.add("open");
     burger.classList.add("active");
     headermenu.classList.add("active");
-    document.querySelector('.header__menu_tab[data-select-item="1"]').classList.add("active");
+    document.querySelector('.header__menu_tab_button[data-select-item="1"]').classList.add("active");
     document.querySelector('.header__menu_list[data-select-item="1"]').classList.add("active");
     document.querySelector('.header__menu_list[data-select-item="1"]').style.maxHeight = document.querySelector('.header__menu_list[data-select-item="1"]').scrollHeight + "px";
   }
